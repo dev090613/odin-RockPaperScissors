b5f557c43bad8f3834935b6ce267980a83b649f4
kokoro <128305501+dev090613@users.noreply.github.com>
2025-07-27 10:07:19 +0900

    complete.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,21 +1,117 @@
+const HUMAN_WIN = "human win";
+const COM_WIN = "computer win";
+const DRAW = "draw";
+
+let humanScore = 0;
+let computerScore = 0;
+let computerChoice = '';
+let humanChoice = '';
+
+function sayResult(result) {
+  switch (result) {
+    case COM_WIN:
+      console.log(`You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`)
+      break;
+    case HUMAN_WIN:
+      console.log(`You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`)
+      break;
+    case DRAW:
+      console.log("DRAW!!!!")
+      break;
+  }
+}
+
+function capitalize(string) {
+  return string.at().toUpperCase() + string.slice(1,)
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor( Math.random() * ( max - min + 1 ) + min )
+}
+
 function getComputerChoice() {
 
-  // Math.random을 활용한다.
-  // 배열은 사용하지 않는다.
-  // 'rock', 'paper', 'scissors' 중 하나를 반환
-  let comChoice = ""
-  const randomNumber = Math.random();
-  // console.log(randomNumber)
+  const randomInt = getRandomInt(1, 3);
   
-  if (randomNumber < 0.33) {
-    comChoice = "rock";
-  } else if( randomNumber < 0.66) {
-    comChoice = "paper"
+  if (randomInt === 1) {
+    computerChoice = "rock";
+  } else if( randomInt === 2) {
+    computerChoice = "paper"
   } else {
-    comChoice = "scissors"
+    computerChoice = "scissors"
   }
-  // console.log(`${randomNumber}, ${comChoice}`)
-  return comChoice;
+  return computerChoice;
 };
 
-getComputerChoice()
+function getHumanChoice() {
+  humanChoice = prompt("Rock, Paper, Sscissors?")
+                      .toLowerCase()
+  return humanChoice;
+}
+
+function playRound(computerChoice, humanChoice) {
+  // "You lose! Paper beats Rock"
+  let result = "";
+  if (computerChoice === 'rock') {
+    switch (humanChoice) {
+      case "rock":
+        result = DRAW;
+        break;
+      case "paper":
+        result = HUMAN_WIN;
+        humanScore++;
+        break;
+      case "scissors":
+        result = COM_WIN;
+        computerScore++;
+        break;
+    }
+  } else if (computerChoice === 'paper') {
+    switch (humanChoice) {
+      case "rock":
+        result = COM_WIN;
+        computerScore++;
+        break;
+      case "paper":
+        result = DRAW;
+        break;
+      case "scissors":
+        result = HUMAN_WIN;
+        humanScore++;
+        break;
+    }
+  } else {
+    switch (humanChoice) {
+      case "rock":
+        result = HUMAN_WIN;
+        humanScore++;
+        break;
+      case "paper":
+        result = COM_WIN;
+        computerScore++;
+        break;
+      case "scissors":
+        result = DRAW;
+        break;
+    }
+  }
+
+  sayResult(result)
+  return [ computerScore, humanScore ];
+}
+
+function playGame(round) {
+  i = 0;
+  while (i < round) {
+    [ computerScore, humanScore ] 
+      = playRound(getComputerChoice(), getHumanChoice())
+    console.log(
+      `com: ${computerScore}, hum: ${humanScore}`
+    );
+    i++;
+  }
+}
+
+playGame(5)
